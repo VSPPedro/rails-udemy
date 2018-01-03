03b5403f3a4dce89a19219b7c5a5425ac7077db3
Pedro Paiva <pedrovsp109@gmail.com>
2018-01-03 05:10:31 +0000

    Aula 036 - Seeds.rb e Rake Tasks

--- notebook/db/seeds.rb
+++ notebook/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Kinds
+puts "Gerando os tipos de contato(Kinds)..."
+Kind.create!([{description: "Amigo"}, 
+            {description: "Comercial"}, 
+            {description: "Contato"}])
+puts "Gerando os tipos de contato(Kinds)... [OK]"
+
+# Contacts
+puts "Gerando os contatos(Contacts)..."
+10.times do |i|
+  Contact.create!(
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    kind: Kind.all.sample, 
+    rmk: Faker::Lorem.paragraph([1,2,3,4,5].sample)
+    )
+end
+puts "Gerando os contatos(Contacts)... [OK]"
